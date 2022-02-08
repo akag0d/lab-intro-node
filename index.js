3c64cdc2599dc9dd9b3517225426569ccd18efb6
akag0d <ruben.neves90@gmail.com>
2022-02-08 21:35:48 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length += 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      var max = Math.max.apply(null, this.items);
+      return max;
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      var min = Math.min.apply(null, this.items);
+      return min;
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((a,b) => a+b)
+    }
 
-  avg() {}
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return (this.items.reduce((a,b) => a+b)) / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
